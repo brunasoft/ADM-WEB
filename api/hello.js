f8e3397b8cb29539dd14dd7c588e5b1c20d5da37
brunasoft <bruna@softata.com.br>
2025-09-30 09:33:49 -0300

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,3 @@
-export default async function handler() {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { 'content-type': 'application/json' }
-  });
-}
-export default async function handler(req, res) {
+export default function handler(req, res) {
   res.status(200).json({ ok: true, runtime: 'node' });
 }
-
