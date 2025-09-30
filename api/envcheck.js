d704839be705d401d00f32e55c70177c96073257
brunasoft <bruna@softata.com.br>
2025-09-30 10:23:02 -0300

    Update envcheck.js

--- api/envcheck.js
+++ api/envcheck.js
@@ -1,10 +1,10 @@
-// api/envcheck.js
 export default function handler(req, res) {
   res.status(200).json({
     has_DATABASE_URL: !!process.env.DATABASE_URL,
     has_POSTGRES_URL: !!process.env.POSTGRES_URL,
     has_NEON_DATABASE_URL: !!process.env.NEON_DATABASE_URL,
     has_POSTGRES_URL_NON_POOLING: !!process.env.POSTGRES_URL_NON_POOLING,
+    has_ARMAZENAR_URL: !!process.env.ARMAZENAR_URL,
     has_MIGRATE_TOKEN: !!process.env.MIGRATE_TOKEN,
     node_env: process.env.NODE_ENV
   });
