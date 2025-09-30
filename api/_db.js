f9f1a245bdcc928ee798449fec4ebd6a425fba9c
brunasoft <bruna@softata.com.br>
2025-09-30 10:22:50 -0300

    Update _db.js

--- api/_db.js
+++ api/_db.js
@@ -5,8 +5,9 @@ const URL =
   process.env.DATABASE_URL ||
   process.env.POSTGRES_URL ||
   process.env.NEON_DATABASE_URL ||
-  process.env.POSTGRES_URL_NON_POOLING;
+  process.env.POSTGRES_URL_NON_POOLING ||
+  process.env.ARMAZENAR_URL; // caso tenha usado esse prefixo na integração
 
-if (!URL) throw new Error('Set DATABASE_URL/POSTGRES_URL in Vercel.');
+if (!URL) throw new Error('Set POSTGRES_URL / DATABASE_URL in Vercel.');
 
 export const sql = neon(URL);
